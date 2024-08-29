0f19717c302e7a01c23cfbcd7250ab26fbb765bf
phuyalg <phuyalg@berea.edu>
2024-08-29 11:50:29 -0400

    Created basics for User class.

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-# I wrote this. 
+class User:
+    def __init__(self, name, clearance):
+        self.name = name
+        self.clearance = clearance
+    def __str__(self):
+        return f"{self.name}({self.clearance})"
+def main():
+    user1 = User("Larry", "public")
+    print(user1)
+
+main()
